52d6509b8eb3999c048160b140d85f46733cf9c5
[anthonymuthondu98] <anthonymuthondu98@gmail.com>
2026-03-22 07:53:47 +0300

    product details screen ui finished

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -48,6 +48,8 @@ export default function Home() {
               <View key={index} className='relative w-full h-48 bg-gray-200 overflow-hidden' style={{width: width - 32}}>
                 <Image source={{uri: banner.image}} className='w-full h-full' resizeMode='cover'/>
 
+                <View className='absolute inset-0 bg-black/40'/>
+
                 <View className='absolute bottom-4 left-4 z-10'>
                   <Text className='text-white text-2xl font-bold'>{banner.title}</Text>
                   <Text className='text-white text-sm font-medium'>{banner.subtitle}</Text>
@@ -55,7 +57,7 @@ export default function Home() {
                     <Text className='text-primary font-bold text-xs'>Get Now</Text>
                   </TouchableOpacity>
                 </View>
-                <View className='absolute inset-0 bg-black/40'/>
+
               </View>
             ))}
           </ScrollView>
--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,6 +1,19 @@
 import { Stack } from "expo-router";
 import '@/global.css';
+import { CartProvider } from "@/context/CartContext";
+import { WishlistProvider } from "@/context/WishlistContext";
+import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown: false}}/>;
+  return (
+    <CartProvider>
+      <WishlistProvider>
+        <Stack screenOptions={{headerShown: false}}/>
+        <Toast />
+      </WishlistProvider>
+    </CartProvider>
+  )
+  
+  
+
 }
